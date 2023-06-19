6abc578ef517f52e3037bf88f12d829cac98a89d
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-19 12:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	  const prices = table.querySelectorAll(".price");
+      let totalPrice = 0;
+  
+      prices.forEach(price => {
+        totalPrice += parseInt(price.textContent);
+      });
+  
+      const totalRow = document.createElement("tr");
+      const totalCell = document.createElement("td");
+      totalCell.setAttribute("colspan", "2");
+      totalCell.textContent = "Total Price: " + totalPrice;
+  
+      totalRow.appendChild(totalCell);
+      table.appendChild(totalRow);
   
 };
 
